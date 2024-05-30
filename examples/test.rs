cdb4ec3a73cff24920aa86cbb283acda0e8ca2fd
Vanta_1 <mcoopersandys@gmail.com>
2024-05-30 01:26:20 -0400

    i lied :]

--- examples/test.rs
+++ examples/test.rs
@@ -5,8 +5,8 @@ use renoired::prelude::*;
 fn main() {
     println!("WASD to move, Press Ctrl+C or Q to close this game!");
 
-    let mut player = (4., 5.);
-    let speed = 3.;
+    let mut player: (f64, f64) = (4., 5.);
+    let speed: f64 = 3.;
 
     let mut game = RenoiredApp::new();
 
@@ -17,19 +17,9 @@ fn main() {
             flow.close();
         }
 
-        if input.pressed(Key::W) {
-            player.1 += speed * time.delta_time()
-        }
-        if input.pressed(Key::S) {
-            player.1 -= speed * time.delta_time()
-        }
-        if input.pressed(Key::A) {
-            player.0 -= speed * time.delta_time()
-        }
-        if input.pressed(Key::D) {
-            player.0 += speed * time.delta_time()
-        }
+        player.0 += ((input.pressed(Key::D) as i32 - input.pressed(Key::A) as i32) as f64) * speed * time.delta_time();
+        player.1 += ((input.pressed(Key::W) as i32 - input.pressed(Key::S) as i32) as f64) * speed * time.delta_time();
 
-        //println!("X: {}, Y: {}", player.0, player.1);
+        println!("X: {}, Y: {}", player.0, player.1);
     })
 }
